34b11fd1285c868ce3907b9e7b1a265629d312b1
minwoo_park <nwpark111@gmail.com>
2025-10-27 02:36:00 +0900

    feat<Winner>: implement countWinnerMove
    4-1.  최종 우승이 될 전진 횟수 확인.

---
+++ src/main/java/racingcar/WinnerSelector.java
@@ -0,0 +1,17 @@
+package racingcar;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+public class WinnerSelector {
+
+    public static String winnerNames(Map<String, Integer> resultMap) {
+
+        return null;
+    }
+
+    public static int countWinnerMove(Map<String, Integer> resultMap) {
+        return Collections.max(resultMap.values());
+    }
+}
