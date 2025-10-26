815dc9f2d7c62904bdcb2a2f4e270e1f2a2678c2
minwoo_park <nwpark111@gmail.com>
2025-10-27 02:19:04 +0900

    feat<Racing>: implement runRacing
    3-3. 전진을 시도할 횟수만큼 반복.

--- src/main/java/racingcar/StartRacing.java
+++ src/main/java/racingcar/StartRacing.java
@@ -1,16 +1,16 @@
 package racingcar;
 
-import camp.nextstep.edu.missionutils.Randoms;
-
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class StartRacing {
 
     public static Map<String, Integer> runRacing(List<String> carNamesList, int moveCount) {
-
-        return null;
+        RaceResult raceResult = new RaceResult(carNamesList);
+        for (int i = 0; i < moveCount; i++) {
+            raceResult.moveOneResult();
+        }
+        return raceResult.getRaceResult();
     }
 
 }
