2801d169798dca6c1eead73be1fdd67bf1dde04c
minwoo_park <nwpark111@gmail.com>
2025-10-26 23:19:17 +0900

    refactor<split>: inline readLine
    main에서 readLine에 대한 불필요한 변수 제거.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -8,8 +8,10 @@ public class Application {
     public static void main(String[] args) {
         // 1. 경주할 자동차 이름
         System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)");
-        String carNamesString = Console.readLine();
-        List<String> carNamesList = CarNameSplitter.splitCarNames(carNamesString);
+        List<String> carNamesList = CarNameSplitter.splitCarNames(Console.readLine());
+
+        // 2. 시도 횟수
+        System.out.println("시도할 횟수는 몇 회인가요?");
 
     }
 }
