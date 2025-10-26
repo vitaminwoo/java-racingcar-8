e6294333db24ad1d791d186df48362f73d228b97
minwoo_park <nwpark111@gmail.com>
2025-10-27 02:51:45 +0900

    style<Main>: remove comment
    메인 메서드에서의 불필요한 주석 삭제

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -7,19 +7,15 @@ import java.util.Map;
 
 public class Application {
     public static void main(String[] args) {
-        // 1. 경주할 자동차 이름
         System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)");
         List<String> carNamesList = CarNameSplitter.splitInput(Console.readLine());
 
-        // 2. 시도 횟수
         System.out.println("시도할 횟수는 몇 회인가요?");
         int moveCount = MoveCountConverter.convertInput(Console.readLine());
 
-        // 3. 경주 진행
         System.out.println("실행 결과");
         Map<String, Integer> raceResult = StartRacing.runRacing(carNamesList, moveCount);
 
-        // 4. 최종 우승자
         System.out.println("최종 우승자 : " + WinnerSelector.winnerNames(raceResult));
     }
 }
