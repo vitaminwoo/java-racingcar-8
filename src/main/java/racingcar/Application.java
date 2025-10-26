0d6637fdfe0c9c19e5433e827fc11800df44d177
minwoo_park <nwpark111@gmail.com>
2025-10-26 23:11:42 +0900

    feat<split>: implement validateCarName, nullCheckCarNames
    1-2. 각각의 자동차 이름이 5자 이하인지, null이 아닌지 검사. 아닐 시 예외 발생.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -9,7 +9,6 @@ public class Application {
         // 1. 경주할 자동차 이름
         System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)");
         String carNamesString = Console.readLine();
-
         List<String> carNamesList = CarNameSplitter.splitCarNames(carNamesString);
 
     }
--- src/main/java/racingcar/CarNameSplitter.java
+++ src/main/java/racingcar/CarNameSplitter.java
@@ -6,6 +6,8 @@ import java.util.List;
 public class CarNameSplitter {
 
     public static List<String> splitCarNames(String carNames) {
+        nullCheckCarNames(carNames);
+
         List<String> carNamesList = new ArrayList<>();
         for (String carName : carNames.split(",")) {
             carNamesList.add(validateCarName(carName));
@@ -13,9 +15,19 @@ public class CarNameSplitter {
         return carNamesList;
     }
 
-    //예외 처리 검증은 여기서.
     public static String validateCarName(String carName) {
-
+        if (carName.length() > 5) {
+            throw new IllegalArgumentException("carName only contains 5 characters");
+        } else if (carName.isEmpty()) {
+            throw new IllegalArgumentException("carName is empty");
+        }
         return carName;
     }
+
+    public static void nullCheckCarNames(String carNames) {
+        if (carNames == null) {
+            throw new IllegalArgumentException("carNames is null");
+        }
+    }
+
 }
