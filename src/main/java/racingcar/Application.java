dfa82e74fad6e22a4fd0643f43ceb3d7a4334946
minwoo_park <nwpark111@gmail.com>
2025-10-27 12:38:30 +0900

    refactor<RaceCount>: rename Main variable
    메인 메서드에서 재정의를 놓친 변수명 moveCount를 raceCount로 수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -11,10 +11,10 @@ public class Application {
         List<String> carNamesList = CarNameSplitter.splitInput(Console.readLine());
 
         System.out.println("시도할 횟수는 몇 회인가요?");
-        int moveCount = RaceCountConverter.convertInput(Console.readLine());
+        int raceCount = RaceCountConverter.convertInput(Console.readLine());
 
         System.out.println("실행 결과");
-        Map<String, Integer> raceResult = StartRacing.runRacing(carNamesList, moveCount);
+        Map<String, Integer> raceResult = StartRacing.runRacing(carNamesList, raceCount);
 
         System.out.println("최종 우승자 : " + WinnerSelector.winnerNames(raceResult));
     }
